9a972c30a6be4a4ae80ad2ebf270304d59d57fa3
foxriver <foxriver660@gmail.com>
2023-08-30 20:36:21 +0300

    feat: add ecosystem config for frontend

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      script: "mesto-frontend",
+    },
+  ],
+
+  // Deployment Configuration
+  deploy: {
+    production: {
+      user: "foxriver",
+      host: "158.160.59.209",
+      ref: "origin/master",
+      repo: "https://github.com/foxriver660/web-plus-pm2-deploy.git",
+      path: "/home/foxriver/frontend",
+      "post-deploy":
+        "cd /home/foxriver/frontend/source/frontend && npm i && npm run build",
+    },
+  },
+};
