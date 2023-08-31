70e111bf5d47158e0ac85b9263d82e9e0f92a221
foxriver <foxriver660@gmail.com>
2023-08-31 10:37:36 +0300

    test 6

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       ref: "origin/master",
       repo: "https://github.com/foxriver660/web-plus-pm2-deploy.git",
       path: "/home/foxriver/frontend",
-      "post-deploy": `cd /home/foxriver/frontend/source/frontend/ && pwd && npm i && npm run build`,
+      "post-deploy": `cd /home/foxriver/mesto-frontend/source/frontend/ && pwd && npm i && npm run build`,
     },
   },
 };
